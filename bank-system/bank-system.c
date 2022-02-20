29c2b1734682944776dfd566f24c31034acdaaa6
kailash1198 <kailash414243@gmail.com>
2022-02-20 22:01:20 +0530

    bank system project code file create and some code written

---
+++ bank-system/bank-system.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//function declaration
+void header();
+void greetingMessage();
+void createAccount();
+void LogIn();
+void balanceStatus();
+void moneyTransfer();
+void cashWithrawal();
+void updateSetting();
+
+//main function start from here
+
+void main()
+{
+    header();
+};
+
+//function declaration here
+
+void header()
+{
+    system("cls");
+    printf("\n\n");
+    printf("\t\t\ HDFC Bank, Harohalli ");
+    printf("\n");
+}
